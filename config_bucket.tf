0a7e957d2a00843ba85bc6ad6f8c97e68245e484
hrushikesh <hrushi2002j@gmail.com>
2024-01-26 12:45:00 +0530

    working: config_bunket: refactor code to use for_each

--- config_bucket.tf
+++ config_bucket.tf
@@ -12,39 +12,46 @@ output "config_bucket_name" {
   value = google_storage_bucket.config_bucket.name
 }
 
+resource "google_storage_bucket_object" "scripts" {
+  for_each = fileset("${path.module}/scripts", "*")
 
-resource "google_storage_bucket_object" "common" {
-  name   = "common.sh"
-  source = "./scripts/common.sh"
+  name   = each.value
+  source = "./scripts/${each.value}"
   bucket = google_storage_bucket.config_bucket.name
 }
 
-resource "google_storage_bucket_object" "master" {
-  name   = "master.sh"
-  source = "./scripts/master.sh"
-  bucket = google_storage_bucket.config_bucket.name
-}
+# resource "google_storage_bucket_object" "common" {
+#   name   = "common.sh"
+#   source = "./scripts/common.sh"
+#   bucket = google_storage_bucket.config_bucket.name
+# }
 
-resource "google_storage_bucket_object" "edge" {
-  name   = "edge.sh"
-  source = "./scripts/edge.sh"
-  bucket = google_storage_bucket.config_bucket.name
-}
+# resource "google_storage_bucket_object" "master" {
+#   name   = "master.sh"
+#   source = "./scripts/master.sh"
+#   bucket = google_storage_bucket.config_bucket.name
+# }
 
-resource "google_storage_bucket_object" "echo_yaml" {
-  name   = "echo.yaml"
-  source = "./scripts/echo.yaml"
-  bucket = google_storage_bucket.config_bucket.name
-}
+# resource "google_storage_bucket_object" "edge" {
+#   name   = "edge.sh"
+#   source = "./scripts/edge.sh"
+#   bucket = google_storage_bucket.config_bucket.name
+# }
 
-resource "google_storage_bucket_object" "master_edgemesh" {
-  name   = "master_edgemesh.sh"
-  source = "./scripts/master_edgemesh.sh"
-  bucket = google_storage_bucket.config_bucket.name
-}
+# resource "google_storage_bucket_object" "echo_yaml" {
+#   name   = "echo.yaml"
+#   source = "./scripts/echo.yaml"
+#   bucket = google_storage_bucket.config_bucket.name
+# }
 
-resource "google_storage_bucket_object" "edge_edgemesh" {
-  name   = "edge_edgemesh.sh"
-  source = "./scripts/edge_edgemesh.sh"
-  bucket = google_storage_bucket.config_bucket.name
-}
+# resource "google_storage_bucket_object" "master_edgemesh" {
+#   name   = "master_edgemesh.sh"
+#   source = "./scripts/master_edgemesh.sh"
+#   bucket = google_storage_bucket.config_bucket.name
+# }
+
+# resource "google_storage_bucket_object" "edge_edgemesh" {
+#   name   = "edge_edgemesh.sh"
+#   source = "./scripts/edge_edgemesh.sh"
+#   bucket = google_storage_bucket.config_bucket.name
+# }
